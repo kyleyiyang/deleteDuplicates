e4bd29658001cf95c06a0eb278a7cd4a10c19f39
kyleyiyang <kyleyiyang@gmail.com>
2021-01-09 23:14:23 -0500

    Update deleteDuplicates.java

--- deleteDuplicates.java
+++ deleteDuplicates.java
@@ -10,40 +10,14 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode temp=new ListNode();
-        ArrayList<ListNode> arr = new ArrayList<>();
-        arr.add(head);
-        while (head!=null) {
-            //System.out.println("arr.size="+arr.size());
-            //System.out.println("head.val="+head.val);
-            //System.out.println("arr.last="+arr.get(arr.size()-1).val);
-            //System.out.println(head.val!=(arr.get(arr.size()-1).val));
-            if (head.val!=(arr.get(arr.size()-1).val)) arr.add(head);
-            if (head.val==(arr.get(arr.size()-1).val)) {
-                head=head.next;
+        ListNode temp=head;
+        while (temp!=null && temp.next!=null) {
+            if (temp.val==temp.next.val) {
+                temp.next=temp.next.next;
             } else {
-                arr.add(head);
-                head=head.next;
+                temp=temp.next;
             }
-            //head=head.next;
         }
-        System.out.println("arr.size="+arr.size());
-        temp = arr.get(arr.size()-1);
-        System.out.println(temp.val);
-        for (int i=1;i<arr.size();i++) {
-            System.out.println("arr.i="+arr.get(i).val);
-            System.out.println(arr.get(arr.size()-i-1).val);
-            temp=new ListNode(arr.get(arr.size()-i-1).val, temp);
-            System.out.println("temp.val="+temp.val);
-        }
-        return temp;
-        /*head = arr.get(arr.size()-1);
-        System.out.println(head.val);
-        for (int i=1;i<arr.size();i++) {
-            System.out.println(arr.get(arr.size()-i-1).val);
-            head=new ListNode(arr.get(arr.size()-i-1).val, head);
-            System.out.println("head.val="+head.val);
-        }
-        return head;*/
+        return head;
     }
 }
